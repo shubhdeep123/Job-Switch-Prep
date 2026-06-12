2880075638f7b5bacdca9e4c489e4cfe23bd8091
Shubhdeep Sharma <shubhdeepsharma213@gmail.com>
2026-06-12 12:16:41 +0530

    cart-context

---
+++ Month 1/Week 2/Day 1/vite-project/src/context/CartContext.tsx
@@ -0,0 +1,67 @@
+import React, { createContext, useContext, useState } from "react";
+
+// define shape of object that context will provide to component
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  rating: number;
+}
+
+interface CartContextType {
+  items: CartItem[];
+  addItem: (item: Omit<CartItem, "id">) => void;
+  removeItem: (id: number) => void;
+  clearCart: () => void;
+  totalItems: number;
+}
+
+// create context
+const CartContext = createContext<CartContextType | null>(null);
+
+// create provider
+export function CartContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [items, setItems] = useState<CartItem[]>([]);
+  const totalItems = items.length;
+
+  function addItem(item: Omit<CartItem,"id">) {
+    const id = Date.now();
+    setItems((prev) => [
+      ...prev,
+      {
+        id: id,
+        name: item.name,
+        price: item.price,
+        rating: item.rating,
+      },
+    ]);
+  }
+
+  function removeItem(id: number) {
+    setItems((prev) => prev.filter((item) => item.id != id));
+  }
+
+  function clearCart() {
+    setItems([]);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ items, addItem, removeItem, clearCart, totalItems }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within CartProvider");
+  }
+  return context;
+}

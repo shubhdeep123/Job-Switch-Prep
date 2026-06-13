b464db72414e55c353c23b8e40703bc98283a970
Shubhdeep Sharma <shubhdeepsharma213@gmail.com>
2026-06-13 21:48:57 +0530

    zustand added

---
+++ Month 1/Week 2/Day 1/vite-project/src/store/useCartStore.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  rating: number;
+  quantity: number;
+}
+
+interface CartStore {
+  items: CartItem[];
+
+  addItems: (item: CartItem) => void;
+  removeItem: (id: number) => void;
+  clearCart: () => void;
+}
+
+export const useCartStore = create<CartStore>((set, get) => ({
+  // state
+  items: [],
+  // actions — use set() to update state
+  addItems: (item: CartItem) =>
+    set((state) => {
+      const existing = state.items.find((i) => i.id === item.id);
+      if (existing) {
+        return {
+          items: state.items.map((i) =>
+            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i,
+          ),
+        };
+      }
+      return {
+        items: [
+          ...state.items,
+          {
+            ...item,
+            quantity: 1,
+          },
+        ],
+      };
+    }),
+  removeItem: (id) =>
+    set((state) => {
+      return {
+        items: state.items.filter((i) => i.id !== id),
+      };
+    }),
+  clearCart: () => set({ items: [] }),
+  
+}));

2f1a03bf8b9bc500682c28e89b48388c47f906ab
shubhdeep123 <64921946+shubhdeep123@users.noreply.github.com>
2026-09-19 22:09:53 +0530

    Clarify array declaration comment in 08Arrays.ts
    Updated comment to clarify array declaration in TypeScript.

--- Month 1/Week 1/Day 1 (Typescript)/src/08Arrays.ts
+++ Month 1/Week 1/Day 1 (Typescript)/src/08Arrays.ts
@@ -1,6 +1,6 @@
 // ARRAYS, TUPLES, AND ENUMS
 
-//most common way to declare an array
+//most common way to declare an array in typescript
 let numbers: number[] = [1, 2, 3, 4, 5];
 let characters: string[] = ["a", "b", "c", "d"];
 
